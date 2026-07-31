c0a135c369ab38f3e3a16b8aeb38378d48a5682c
Sunsetz <2867440557ftt@gmail.com>
2026-07-31 17:05:16 +0800

    feat: Electron desktop app + GitHub Actions auto-release for Win/Mac/Linux

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'The Tower',
+    backgroundColor: '#0f1119',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.setMenuBarVisibility(false);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
